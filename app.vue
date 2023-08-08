de6ce6a394e05657f8ee2dc4b59d70538b9660be
hejiayi <hejiayi@kp99.cn>
2023-08-08 18:53:04 +0800

    feat: 测试页面

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+
+    123
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'NuxtDemo',
+  meta: [
+    { name: 'description', content: 'some description' }
+  ],
+  script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
+})
+</script>

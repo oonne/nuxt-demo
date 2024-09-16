c9b24be4f247234f7e350632b65ebc75724b4d47
JAY <JAY@oonne.com>
2024-09-16 16:31:33 +0800

    chore: 端口修改

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devServer: {
+    port: 7777,
+  },
+  devtools: {
+    enabled: true
+  },
   modules: [
     '@nuxtjs/i18n',
     '@nuxtjs/eslint-module'
   ],
-
-  devtools: { enabled: true },
   i18n: {
     vueI18n: './locales/index.ts',
   },
-
   compatibilityDate: '2024-09-16',
 });

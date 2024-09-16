7f05a035e032e0e461e429a76b70719cd0d24b0a
JAY <JAY@oonne.com>
2024-09-16 16:42:20 +0800

    feat: useSEO

--- app.vue
+++ app.vue
@@ -14,6 +14,15 @@ useHead({
     { innerHTML: 'console.log(\'ArogFly!\')' }
   ]
 });
+
+useSeoMeta({
+  title: 'ArogFly',
+  ogTitle: 'ArogFly',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+});
 </script>
 
 <style>
